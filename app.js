59e3820c5a85eed15d20e03c9d9f7d82cf708500
moizweb <moizshaikh0865@gmail.com>
2022-11-17 00:04:25 +0530

    started with product model

--- app.js
+++ app.js
@@ -29,11 +29,13 @@ app.use(morgan('tiny'))
 //import all routes here
 const home = require("./routes/home")
 const user = require("./routes/userRoute");
+const product = require("./routes/productRoute");
 
 
 //router middleware
 app.use("/api/v1", home)
 app.use("/api/v1", user)
+app.use("/api/v1", product)
 
 
 //export app js
